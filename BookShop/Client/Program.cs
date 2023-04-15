3328f9a950436867102177a4710b01593c8f6627
Roman Permiakov <info@presstomsk.ru>
2023-04-15 15:41:54 +0700

    Add LocaleStorage

--- BookShop/Client/Program.cs
+++ BookShop/Client/Program.cs
@@ -1,7 +1,11 @@
+using Blazored.LocalStorage;
+using Blazored.Toast;
 using BookShop.Client.Services.CategoryService;
 using BookShop.Client.Services.ProductService;
+using BookShop.Client.Services.ToastService;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
 
 namespace BookShop.Client
 {
@@ -16,6 +20,10 @@ namespace BookShop.Client
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<IProductService, ProductService>();
             builder.Services.AddScoped<ICategoryService, CategoryService>();
+            builder.Services.AddScoped<IToastService, ToastService>();
+            builder.Services.AddBlazoredLocalStorage();
+            builder.Services.AddBlazoredToast();
+            
 
             await builder.Build().RunAsync();
         }
---
+++ BookShop/Client/Services/ToastService/IToastService.cs
@@ -0,0 +1,9 @@
+﻿namespace BookShop.Client.Services.ToastService
+{
+    public interface IToastService
+    {
+        event Action<string, ToastLevel>? OnShow;
+        event Action? OnHide;
+        void ShowToast(string message, ToastLevel level);
+    }
+}
