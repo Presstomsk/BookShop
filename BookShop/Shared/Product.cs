16b275202d8f853f98e806d6678ae2093a06148c
Roman Permiakov <info@presstomsk.ru>
2023-04-23 17:31:40 +0700

    Update Product model

--- BookShop/Shared/Product.cs
+++ BookShop/Shared/Product.cs
@@ -8,8 +8,7 @@ namespace BookShop.Shared
         public int Id { get; set; }
         public string? Title { get; set; }
         public string? Description { get; set; }
-        public string Image { get; set; } = "https://via.placeholder.com/300x300";
-        public bool IsPublic { get; set; }
+        public string Image { get; set; } = "https://via.placeholder.com/300x300";        
         public bool IsDeleted { get; set; }
         public Category? Category { get; set; }
         public int CategoryId { get; set; }
